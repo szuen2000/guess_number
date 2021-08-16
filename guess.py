f3754478cd6f2f0baa0d6c9594fb3fc61e3afc2d
Chan <match0219m@gmail.com>
2021-08-16 17:19:04 +0800

    second

--- guess.py
+++ guess.py
@@ -1,7 +1,8 @@
 import random
 r = random.randint(  1 , 100 )
-
+count = 0 :
 while True:
+	count += 1
 	user = int(input('猜數字: '))
 	if user == r :
 		print('恭喜你猜對了! ')
@@ -10,3 +11,4 @@ while True:
 		print('比答案大')
 	else :
 		print ('比答案小')
+	print('這是你猜的', count , '次')
